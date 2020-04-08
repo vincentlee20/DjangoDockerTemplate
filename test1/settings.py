c29d24f5decd3519a23ba8467da41001ce26995f
csvinlee <csvinlee@gmail.com>
2020-04-08 23:23:20 +0000

    final version

--- test1/settings.py
+++ test1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_d$0302=k)g4hk@u7za)!i(2+n3)49obhy@b!pm=z2o1!nu!n0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
